af4498e06dcdb64aa854db68abc81d1e450acb2b
pinhobrunodev <iaishowcs@gmail.com>
2021-11-01 19:03:29 -0300

    Configuring Eureka Server

--- hr-eureka-server/src/main/java/com/pinhobrunodev/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/pinhobrunodev/hreurekaserver/HrEurekaServerApplication.java
@@ -2,8 +2,12 @@ package com.pinhobrunodev.hreurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+// Habilitando o servidor Eureka
 @SpringBootApplication
+@EnableEurekaServer
 public class HrEurekaServerApplication {
 
 	public static void main(String[] args) {
