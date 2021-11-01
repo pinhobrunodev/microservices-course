d956bad279b88e9fe9ec63cf83fb9fe3d4ecf33a
pinhobrunodev <iaishowcs@gmail.com>
2021-11-01 15:42:02 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/pinhobrunodev/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/pinhobrunodev/hrpayroll/services/PaymentService.java
@@ -1,15 +1,36 @@
 package com.pinhobrunodev.hrpayroll.services;
 
-import org.springframework.stereotype.Service;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pinhobrunodev.hrpayroll.entities.Payment;
+import com.pinhobrunodev.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 
+	@Value("${hr-worker.host}")
+	private String workerHost;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Autowired
+	ObjectMapper mapper;
+
 	// a partir do workerId que eu passar , vou precisar chamar ele la no hr-worker
-	public Payment getPayment(Long workerId,int days) {
-		// Pagamento mockado
-		return new Payment("Bob",200.00,days);
+	public Payment getPayment(Long workerId, int days) {
+		
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", ""+workerId);
+		
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
